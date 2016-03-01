e18609f43fc517a5b37bde6573a3372f876d50ed
P Park <paul-sthings@idealworldinc.com>
2016-03-01 06:40:45 -0500

    Running State

---
+++ smartapps/xppppp/unlock-events.src/unlock-events.groovy
@@ -0,0 +1,71 @@
+/**
+ *  Unlock Events
+ *
+ *  Copyright 2016 P Park
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Unlock Events",
+    namespace: "xppppp",
+    author: "P Park",
+    description: "Do Some Stuff when Unlocking",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("When this lock is unlocked") {
+		input "lock1", "capability.lock", multiple: false, required: true
+	}
+}
+
+def installed() {
+	subscribe(lock1, "lock.unlocked", unlockedHandler)
+}
+
+def updated() {
+	unsubscribe()
+	subscribe(lock1, "lock.unlocked", unlockedHandler)
+}
+
+// TODO: implement event handlers
+private def executeRoutine(name) {
+    log.trace "Executing Routine \'${name}\'"
+    location.helloHome.execute(name)
+}
+
+private def setMode(name) {
+    log.trace "Setting location mode to \'${name}\'"
+    setLocationMode(name)
+}
+
+private def setAlarmMode(name) {
+    log.trace "Setting alarm system mode to \'${name}\'"
+
+    def event = [
+        name:           "alarmSystemStatus",
+        value:          name,
+        isStateChange:  true,
+        displayed:      true,
+        description:    "alarm system status is ${name}",
+    ]
+
+    sendLocationEvent(event)
+}
+
+def unlockedHandler(evt) {
+	log.debug evt.value
+	log.debug "Turning off alarm"
+   	setAlarmMode("off")
+}
